6a17933b93d2d4e14b9210add31256e9192fef7c
Juan Manuel Berros <juanmaberros@gmail.com>
2017-03-28 17:48:31 -0300

    depth of coverage renames temp files

--- paip/pipelines/quality_control/depth_of_coverage.py
+++ paip/pipelines/quality_control/depth_of_coverage.py
@@ -1,3 +1,5 @@
+import os
+
 from paip.pipelines.variant_calling import RecalibrateAlignmentScores
 from paip.task_types import SampleTask
 from paip.helpers.create_cohort_task import create_cohort_task
@@ -11,17 +13,29 @@ class DepthOfCoverage(SampleTask):
     OUTPUT = 'depth_of_coverage'
 
     def run(self):
-        with self.output().temporary_path() as outfile:
+        with self.output().temporary_path() as tempfile:
             program_name = 'gatk DepthOfCoverage'
             program_options = {
                 'input_bam': self.input().fn,
-                'outfile': outfile,
+                'outfile': tempfile,
             }
             self.run_program(program_name, program_options)
 
+        # A whole bunch of extra files are created using the original
+        # output filename as a base:
+        extra_files = [
+            '.sample_cumulative_coverage_counts',
+            '.sample_cumulative_coverage_proportions',
+            '.sample_interval_statistics',
+            '.sample_interval_summary',
+            '.sample_statistics',
+            '.sample_summary',
+        ]
 
-            ### SEGUIR ACA ###
-            # Deal with the depth_of_coverage accessory files!
+        for extra_extension in extra_files:
+            temp_filename = tempfile + extra_extension
+            intended_filename = self.output().fn + extra_extension
+            os.rename(temp_filename, intended_filename)
 
 
 DepthOfCoverageCohort = create_cohort_task(DepthOfCoverage)
