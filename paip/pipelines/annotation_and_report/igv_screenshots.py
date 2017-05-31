3a8a50612fb56bf07827e8b753f35b4df6e084fd
Juan Manuel Berros <juanmaberros@gmail.com>
2017-05-31 18:56:15 -0300

    WIP igv screenshots

---
+++ paip/pipelines/annotation_and_report/igv_screenshots.py
@@ -0,0 +1,8 @@
+from paip.task_types import SampleTask
+
+
+class IGVScreenshots(SampleTask):
+    """
+    Takes a screenshot of the pile of reads in IGV for each of the variants
+    in a sample VCF.
+    """
