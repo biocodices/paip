ad91149749dddecf3ceac4136ee62589789b4e26
Juan Manuel Berros <juanmaberros@gmail.com>
2019-01-08 17:15:36 -0300

    Deprecated some old annotation tasks

--- paip/pipelines/annotation_and_report/__init__.py
+++ paip/pipelines/annotation_and_report/__init__.py
@@ -4,6 +4,6 @@ from .annotate_variants import AnnotateVariants
 from .annotate_genes import AnnotateGenes, extract_entrez_gene_ids_from_vep_tsv
 from .generate_reports import GenerateReports
 from .take_igv_snapshots import TakeIGVSnapshots
-from .annotate_with_clinvar import AnnotateWithClinvar
-from .annotate_with_dbsnp_web import AnnotateWithDbsnpWeb
-from .annotate_with_gene_entrez import AnnotateWithGeneEntrez
+# from .annotate_with_clinvar import AnnotateWithClinvar
+# from .annotate_with_dbsnp_web import AnnotateWithDbsnpWeb
+# from .annotate_with_gene_entrez import AnnotateWithGeneEntrez
