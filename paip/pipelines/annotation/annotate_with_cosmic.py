fe061cf3a4464e6736285f8bb024998b619e0cfc
Juan Manuel Berros <juanmaberros@gmail.com>
2019-01-11 18:07:15 -0300

    Annotate With Cosmic

--- paip/pipelines/annotation/annotate_with_cosmic.py
+++ paip/pipelines/annotation/annotate_with_cosmic.py
@@ -1,5 +1,13 @@
+import re
+import logging
+
+import pandas as pd
+
 from paip.task_types import CohortTask
 from paip.pipelines.annotation import AnnotateWithClinvarVcf
+
+
+logger = logging.getLogger(__name__)
 
 
 class AnnotateWithCosmic(CohortTask):
@@ -11,10 +19,80 @@ class AnnotateWithCosmic(CohortTask):
     OUTPUT_RENAMING = ('.vcf', '.COS.vcf')
 
     def run(self):
-        with self.output().temporary_path() as temp_vcf:
-            program_name = 'snpsift COSMIC'
-            program_options = {
-                'input_vcf': self.input().path,
-            }
-            self.run_program(program_name, program_options,
-                             redirect_stdout_to_path=temp_vcf)
+        temp_vcf_1 = self.output().path.replace('.vcf', '-temp.vcf')
+        program_name = 'snpsift COSMIC'
+        program_options = {
+            'input_vcf': self.input().path,
+        }
+        self.run_program(program_name, program_options,
+                         redirect_stdout_to_path=temp_vcf_1)
+
+        with self.output().temporary_path() as temp_vcf_2:
+            self.add_COSMIC_INFO(temp_vcf_1,
+                                 temp_vcf_2,
+                                 self.config.resources['cosmic_mutation_data'])
+
+
+    def add_COSMIC_INFO(self, vcf, new_vcf, cosmic_data_file):
+        '''
+        Add COSMIC INFO to the VCF lines where a COSMIC ID is found.
+        Write the result to a *new_vcf*.
+        '''
+        cosmic_df = self.read_COSMIC_mutation_info(path=cosmic_data_file)
+
+        # Use any line to create the meta information for the VCF:
+        cosmic_meta_line = self.matches_to_INFO(cosmic_df.loc[0:0])[0]
+
+        new_lines = []
+
+        logger.info(f'Reading and parsig: {vcf}')
+        last_line_was_INFO = False
+        with open(vcf) as f:
+            for line in f:
+                line = line.strip()
+                this_line_is_INFO = line.startswith('##INFO')
+
+                if last_line_was_INFO and not this_line_is_INFO:
+                    new_lines.append(cosmic_meta_line + "\n")
+
+                is_genotype = not line.startswith('#')
+                if is_genotype:
+                    fields = line.split("\t")
+                    ids = fields[2].split(';')
+                    cosmic_ids = [id_ for id_ in ids if re.search(r'^COSM\d+$', id_)]
+                    matches = cosmic_df[cosmic_df['Mutation ID'].isin(cosmic_ids)]
+                    if len(matches):
+                        extra_info_chunk = self.matches_to_INFO(matches)[1]
+                        fields[7] = fields[7] + extra_info_chunk
+                        line = "\t".join(fields)
+
+                new_lines.append(line + "\n")
+
+                last_line_was_INFO = this_line_is_INFO
+
+        logger.info(f'Writing to: {new_vcf}')
+        with open(new_vcf, 'w') as f:
+            for line in new_lines:
+                f.write(line)
+
+    def read_COSMIC_mutation_info(self, path):
+        return pd.read_table(path)
+
+    def matches_to_INFO(self, df):
+        '''
+        Transform one or more rows of data from a DataFrame *df* into a tuple
+        of 2 strings: a meta line for the VCF and a chunk that can be put in
+        the VCF INFO field.
+        '''
+        df = df.applymap(lambda value: value.replace(' ', '_') \
+                         if isinstance(value, str) else value)
+
+        fields = '|'.join(df.columns)
+        meta_line = f'##INFO=<ID=COSMIC,Number=1,Type=String,Description="{fields}">'
+
+        values = ['|'.join(str(v) for v in row.values)
+                  for ix, row in df.iterrows()]
+        merged_values = ','.join(values)
+        info_chunk = f';COSMIC={merged_values}'
+
+        return (meta_line, info_chunk)
--- tests/pipelines/annotation/test_annotate_with_cosmic.py
+++ tests/pipelines/annotation/test_annotate_with_cosmic.py
@@ -1,13 +1,67 @@
+from unittest.mock import Mock
+
 import re
+
+import pytest
+import pandas as pd
 
 from paip.pipelines.annotation import AnnotateWithCosmic
 
 
-def test_run(cohort_task_factory):
-    task = cohort_task_factory(AnnotateWithCosmic)
+@pytest.fixture
+def task(cohort_task_factory):
+    return cohort_task_factory(AnnotateWithCosmic)
+
+def test_run(task, monkeypatch):
+    mock_add_COSMIC_INFO = Mock()
+    monkeypatch.setattr(AnnotateWithCosmic, 'add_COSMIC_INFO',
+                        mock_add_COSMIC_INFO)
+
     task.run()
-
     (command, ), kwargs = task.run_command.call_args
-
     assert 'SnpSift.jar annotate -id -noInfo -v' in command
-    assert re.search(r'Cohort1..+.COS.vcf', kwargs['redirect_stdout_to_path'])
+    assert re.search(r'Cohort1..+.COS-temp.vcf',
+                     kwargs['redirect_stdout_to_path'])
+
+    _args = mock_add_COSMIC_INFO.call_args[0]
+    assert _args[0].endswith('COS-temp.vcf')
+    assert re.search(r'Cohort1..+.COS.vcf-luigi-tmp', _args[1])
+    assert _args[2].endswith('/CosmicGenomeScreensMutantExport.tsv')
+
+
+def test_add_COSMIC_INFO(task, tmpdir):
+    vcf = pytest.helpers.file('with_COSMIC_ids.vcf')
+    new_vcf = tmpdir.join('with_COSMIC_INFO.vcf')
+    cosmic_data_file = pytest.helpers.file('CosmicGenomeMutant.tsv')
+
+    task.add_COSMIC_INFO(vcf, new_vcf, cosmic_data_file)
+
+    with open(new_vcf) as f:
+        lines = f.readlines()
+
+    assert lines[2].startswith('##INFO=<ID=COSMIC')
+    assert lines[3].startswith('#CHROM')
+
+    assert 'FOO=BAR;COSMIC=GENE1|1|COSM0001|Confirmed_somatic_variant' in lines[4]
+    assert '.;COSMIC=GENE1|1|COSM0001|Confirmed_somatic_variant,GENE2|2|COSM0002|Confirmed_somatic_variant' in lines[10]
+
+
+def test_matches_to_INFO(task):
+    rows = pd.DataFrame({
+        'Gene name': ['GENE1', 'GENE2'],
+        'Mutation ID': ['COSM1', 'COSM2'],
+        'Mutation somatic status': ['Confirmed somatic', 'Previously observed']
+    })
+    result = task.matches_to_INFO(rows)
+    assert result[0] == '##INFO=<ID=COSMIC,Number=1,Type=String,Description="Gene name|Mutation ID|Mutation somatic status">'
+    assert result[1] == ';COSMIC=GENE1|COSM1|Confirmed_somatic,GENE2|COSM2|Previously_observed'
+
+
+def test_read_COSMIC_mutation_info(task):
+    path = pytest.helpers.file('CosmicGenomeMutant.tsv')
+    result = task.read_COSMIC_mutation_info(path=path)
+    assert len(result) == 2
+    assert result.loc[0]['Gene name'] == 'GENE1'
+    assert result.loc[1]['Gene name'] == 'GENE2'
+    assert result.loc[0]['Mutation ID'] == 'COSM0001'
+    assert result.loc[1]['Mutation ID'] == 'COSM0002'
